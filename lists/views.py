cc3574ac0bcc36832ce1143b46745a53b759cfff
Cheena <c.bazarbai@gmail.com>
2022-02-02 15:04:49 +0600

    Created a List model, adjusted new_list view

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
@@ -11,6 +10,8 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
+    list_ = List.objects.create()
     item_text = request.POST['item_text']
-    Item.objects.create(text=item_text)
+    Item.objects.create(text=item_text, list=list_)
+
     return redirect('/lists/the-only-list-in-the-world/')
