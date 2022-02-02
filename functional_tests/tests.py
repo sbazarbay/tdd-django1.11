a21a6e821921adacc0f4d672df3d8f0ab216ea2c
Cheena <c.bazarbai@gmail.com>
2022-02-02 14:17:08 +0600

    Changed FT max wait time from 10 to 5

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 import time
 
-MAX_WAIT = 10
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
 
