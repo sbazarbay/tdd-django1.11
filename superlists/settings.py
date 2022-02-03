9ecc117d5dbf3b7eb27531f053ecc6ab3797da6d
Cheena <c.bazarbai@gmail.com>
2022-02-03 16:31:02 +0600

    Hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "qsc21-64lyfeoc5#n75(9@2mutc0zno1v%d5-tn879sn%si6+k"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
