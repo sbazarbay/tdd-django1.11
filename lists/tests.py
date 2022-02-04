44b4032e5fc1d0f8a272a41ddb143a1ddc872138
Cheena <c.bazarbai@gmail.com>
2022-02-04 11:58:21 +0600

    Mini refactor in lists/tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -66,14 +66,14 @@ class ListViewTest(TestCase):
 
 
 class NewListTest(TestCase):
-    def test_can_save_a_POST_request(self):
+    def test_can_save_a_post_request(self):
         self.client.post("/lists/new", data={"item_text": "A new list item"})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
 
-    def test_redirects_after_POST(self):
+    def test_redirects_after_post(self):
         response = self.client.post("/lists/new", data={"item_text": "A new list item"})
         new_list = List.objects.first()
         self.assertRedirects(response, f"/lists/{new_list.id}/")
