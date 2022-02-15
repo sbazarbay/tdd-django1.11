a94d158ff9bd7d48d0c0a301ca19800ae5fddd7c
Cheena <c.bazarbai@gmail.com>
2022-02-15 19:59:01 +0600

    Accidently left deliberate fail call, now removed

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -8,7 +8,7 @@ class LayoutAndStylingTest(FunctionalTest):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-        self.fail()
+
         # She notices the input box is nicely centered
         inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
