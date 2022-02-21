f001cfdcc3d53045672306583bc554b0e9435dbf
Cheena <c.bazarbai@gmail.com>
2022-02-21 10:55:43 +0600

    Added new_list fbv again, because mock tests started failing. Replaced view_list fbv with ViewAndAddToList cbgv

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import redirect, render
-from django.views.generic import CreateView, FormView
+from django.views.generic import CreateView, DetailView, FormView
 
 from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from lists.models import List
@@ -14,16 +14,14 @@ class HomePageView(FormView):
     form_class = ItemForm
 
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ExistingListItemForm(for_list=list_)
-    if request.method == "POST":
-        form = ExistingListItemForm(for_list=list_, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(list_)
+class ViewAndAddToList(DetailView, CreateView):
+    model = List
+    template_name = "list.html"
+    form_class = ExistingListItemForm
 
-    return render(request, "list.html", {"list": list_, "form": form})
+    def get_form(self):
+        self.object = self.get_object()
+        return self.form_class(for_list=self.object, data=self.request.POST)
 
 
 class NewListView(CreateView):
@@ -33,6 +31,14 @@ class NewListView(CreateView):
     def form_valid(self, form: NewListForm):
         list_ = form.save(owner=self.request.user)
         return redirect(list_)
+
+
+def new_list(request: WSGIRequest):
+    form = NewListForm(data=request.POST)
+    if form.is_valid():
+        list_ = form.save(owner=request.user)
+        return redirect(list_)
+    return render(request, "home.html", {"form": form})
 
 
 def my_lists(request, email):
