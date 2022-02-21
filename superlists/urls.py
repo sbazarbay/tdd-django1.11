4265cc1bf1ddf5b6ad393a9581d1a472ffded7ca
Cheena <c.bazarbai@gmail.com>
2022-02-21 10:22:50 +0600

    Run isort

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 
 from accounts import urls as account_urls
-
 # from django.contrib import admin
 from lists import urls as list_urls
 from lists import views as list_views
