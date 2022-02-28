d62da4c63388cba4a7465f5db6018adedeb42ef4
Cheena <c.bazarbai@gmail.com>
2022-02-28 17:11:29 +0600

    added tests to fix error text display

--- lists/tests/test_api.py
+++ lists/tests/test_api.py
@@ -2,11 +2,16 @@ import json
 
 from django.test import TestCase
 
+from lists.forms import DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR
 from lists.models import Item, List
 
 
 class ListAPITest(TestCase):
     base_url = "/api/lists/{}/"
+
+    def post_empty_input(self):
+        list_: List = List.objects.create()
+        return self.client.post(self.base_url.format(list_.pk), data={"text": ""})
 
     def test_get_returns_json_200(self):
         list_: List = List.objects.create()
@@ -36,3 +41,25 @@ class ListAPITest(TestCase):
         self.assertEqual(response.status_code, 201)
         new_item: Item = list_.item_set.get()
         self.assertEqual(new_item.text, "new item")
+
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        self.post_empty_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_for_invalid_input_returns_error_code(self):
+        response = self.post_empty_input()
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.content.decode("utf8")), {"error": EMPTY_ITEM_ERROR}
+        )
+
+    def test_duplicate_items_error(self):
+        list_: List = List.objects.create()
+        self.client.post(self.base_url.format(list_.pk), data={"text": "thing"})
+        response = self.client.post(
+            self.base_url.format(list_.pk), data={"text": "thing"}
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.content.decode("utf8")), {"error": DUPLICATE_ITEM_ERROR}
+        )
